d1fdce12767eacee5e7f7e547412430180680209
Rudolf Radlbauer <radlbauer@spengergasse.at>
2025-10-01 09:47:16 +0200

    test vehicle

---
+++ vehicle/src/main/resources/import.sql
@@ -0,0 +1,17 @@
+insert into vehicle (brand, model)
+values ('Opel', 'Kadett');
+insert into vehicle (brand, model)
+values ('Opel', 'Astra'),
+       ('Opel', 'Corsa'),
+       ('Opel', 'Insignia'),
+       ('VW', 'Käfer'),
+       ('VW', 'Golf'),
+       ('VW', 'Polo'),
+       ('VW', 'Passat'),
+       ('BMW', '3er'),
+       ('BMW', '5er'),
+       ('BMW', '7er'),
+       ('Mercedes', 'A-Klasse'),
+       ('Mercedes', 'C-Klasse'),
+       ('Mercedes', 'E-Klasse'),
+       ('Mercedes', 'S-Klasse');
